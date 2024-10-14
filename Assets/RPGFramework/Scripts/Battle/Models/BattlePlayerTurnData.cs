8f46aa08241e032b385322cf6adab41c3d7a02ff
DioMain <dima123.zample@gmail.com>
2024-10-14 10:10:11 +0300

    feat: added own monobeh

---
+++ Assets/RPGFramework/Scripts/Battle/Models/BattlePlayerTurnData.cs
@@ -0,0 +1,35 @@
+﻿public class BattlePlayerTurnData
+{
+    public bool IsTarget;
+    public bool IsDead;
+
+    public int ReservedConcentration;
+
+    public RPGEnemy EnemyBuffer;
+    public RPGCharacter CharacterBuffer;
+    public RPGEntity EntityBuffer;
+
+    #region ACT INFO
+
+    public bool IsAbility;
+
+    public RPGEnemy.EnemyAct InteractionAct;
+
+    public RPGAbility Ability;
+
+    #endregion
+
+    #region ITEM INFO
+
+    public RPGCollectable Item;
+    public bool IsConsumed;
+
+    #endregion
+
+    #region DEFENCE INFO
+
+    public bool IsDefence;
+    public bool IsFlee;
+
+    #endregion
+}
---
+++ Assets/RPGFramework/Scripts/Other/RPGFrameworkBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RPGFrameworkBehaviour : MonoBehaviour, IManagerInitialize
+{
+    protected GameManager Game => GameManager.Instance;   
+    protected LocalManager Local => LocalManager.Instance;
+    protected ExplorerManager Explorer => ExplorerManager.Instance;
+    protected CommonManager Common => CommonManager.Instance;
+    protected BattleManager Battle => BattleManager.Instance;
+
+    public virtual void Initialize() { }
+}

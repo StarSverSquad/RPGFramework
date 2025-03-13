17374abe3e0f7ecb9e860b59286c10e546121618
DioMain <dima123.zample@gmail.com>
2025-03-13 16:08:38 +0300

    fix: delete obsolete in RPGCCL

--- Assets/RPGFramework/Scripts/Character/RPGCharacterControllerLegacy.cs
+++ Assets/RPGFramework/Scripts/Character/RPGCharacterControllerLegacy.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
-[Obsolete]
 public class RPGCharacterControllerLegacy : MonoBehaviour
 {
     #region NOT UNITY SERIALIZED

4f0c8154b62fd0370edc089407f0e251a26c8044
DioMain <dima123.zample@gmail.com>
2025-03-13 16:59:01 +0300

    feat: added base API

---
+++ Assets/RPGFramework/Scripts/Character/Controller/IRPGCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RPGF.Character
+{
+    public enum Direction
+    {
+        Up, Down, Left, Right
+    }
+
+    /// <summary>
+    /// Describe base API
+    /// </summary>
+    public interface IRPGCharacterController
+    {
+        public void RotateTo(Direction direction);
+
+        public void MoveTo(Vector2 position, float time);
+        public void MoveToRelative(Vector2 offset, float time);
+    }
+}
--- Assets/RPGFramework/Scripts/Character/Controller/RPGCharacterControllerBase.cs
+++ Assets/RPGFramework/Scripts/Character/Controller/RPGCharacterControllerBase.cs
@@ -1,10 +1,121 @@
+using DG.Tweening;
 using System;
 using UnityEngine;
 
-public abstract class RPGCharacterControllerBase : MonoBehaviour, IDisposable
+namespace RPGF.Character
 {
-    public virtual void Dispose()
+    public abstract class RPGCharacterControllerBase : RPGFrameworkBehaviour, IDisposable, IRPGCharacterController
     {
-        
+        [SerializeField]
+        private Direction _startDiration;
+
+        #region PROPS
+
+        public Direction Direction { get; private set; }
+
+        public bool IsMove => moveTween != null;
+
+        #endregion
+
+        #region PRIVATE/PROTECTED FIELDS
+
+        private Tween moveTween = null;
+
+        #endregion
+
+        #region EVENTS
+
+        public event Action OnStartMoveEvent;
+        public event Action OnEndMoveEvent;
+
+        public event Action<Direction> OnRotateEvent;
+
+        #endregion
+
+        public override void Initialize()
+        {
+            RotateTo(Direction);
+        }
+
+        #region MAIN API
+
+        public void RotateTo(Direction direction)
+        {
+            Direction = direction;
+
+            OnRotateEvent?.Invoke(direction);
+            OnRotate(direction);
+        }
+
+        public void MoveTo(Vector2 position, float time)
+        {
+            DisposeMoveTween();
+
+            Vector2 vectorDiretion = (Vector2)transform.position - position;
+
+            Direction moveDiretion = GetDirectionByVector(vectorDiretion);
+
+            moveTween = transform.DOMove(position, time).Play();
+
+            moveTween.onComplete += () =>
+            {
+                OnEndMove();
+                OnEndMoveEvent?.Invoke();
+
+                moveTween = null;
+            };
+            moveTween.onPlay += () =>
+            {
+                OnStartMove();
+                OnStartMoveEvent?.Invoke();
+            };
+        }
+
+        public void MoveToRelative(Vector2 offset, float time)
+        {
+            MoveTo((Vector2)transform.position + offset, time);
+        }
+
+        #endregion
+
+        #region ADDATIVE API
+
+        protected Direction GetDirectionByVector(Vector2 vector)
+        {
+            if (vector.y > 0.20f)
+                return Direction.Up;
+            else if (vector.y < 0.20f)
+                return Direction.Down;
+            else if (vector.x > 0)
+                return Direction.Right;
+            else if (vector.x < 0)
+                return Direction.Left;
+            else
+                return Direction.Down;
+        }
+
+        protected void DisposeMoveTween()
+        {
+            if (moveTween != null)
+            {
+                moveTween.Kill();
+                moveTween = null;
+            }
+        }
+
+        #endregion
+
+        public virtual void Dispose()
+        {
+            DisposeMoveTween();
+        }
+
+        #region VIRTUALS
+
+        protected virtual void OnStartMove() { }
+        protected virtual void OnEndMove() { }
+        protected virtual void OnRotate(Direction direction) { }
+
+        #endregion
     }
 }
